37d1c74a3b89a027075740947d588963f61b38fc
wangyang <229321651@qq.com>
2023-06-27 22:05:35 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+//引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//引入样式
+import './assets/gloable.css';
 Vue.config.productionTip = false
+Vue.use(ElementUI, { size: "small" });
 
 new Vue({
   router,
